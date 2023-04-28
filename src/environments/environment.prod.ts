d5ee1a31fdd5e032702c8849e5ed1c226952cb83
xr-chen <56777910+xr-chen@users.noreply.github.com>
2023-04-28 11:28:09 -0500

    Update environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiEndpoint: 'http://localhost:8080/api/v1/'
+  apiEndpoint: 'http://localhost:3000'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiEndpoint: 'http://localhost:8080/api/v1/'
+  apiEndpoint: 'http://localhost:3000'
 };
 
 /*
